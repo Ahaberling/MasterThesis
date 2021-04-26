085f95d7885249923366e77b51b2ca6bcbea6578
Alexander Haberling <alexanderhaberling@yahoo.de>
2021-04-26 15:27:35 +0200

    ipc appended

---
+++ measureExplo.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle as pk
+
+#--- Initialization --#
+
+directory = 'D:/Universitaet Mannheim/MMDS 7. Semester/Master Thesis/Outline/Data/Cleaning Robots/'
+
+patent_lda_ipc = pd.read_csv( directory + 'patent_lda_ipc.csv', quotechar='"', skipinitialspace=True)
+#topics = pd.read_csv(r'D:\Universitaet Mannheim\MMDS 7. Semester\Master Thesis\Outline\Data\Cleaning Robots\cleaning_robot_EP_patents_07_04_topics.csv', quotechar='"', skipinitialspace=True)
+#parent = pd.read_csv(r'D:\Universitaet Mannheim\MMDS 7. Semester\Master Thesis\Outline\Data\Cleaning Robots\cleaning_robot_EP_backward_citations.csv', quotechar='"', skipinitialspace=True)
+
+patent_lda_ipc = patent_lda_ipc.to_numpy()
+
+
+print(patent_lda_ipc)
+print(np.shape(patent_lda_ipc))
