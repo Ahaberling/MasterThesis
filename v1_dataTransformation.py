3f4d996f355c39fa1d42e922d24f5f6b6defccd2
Alexander Haberling <alexanderhaberling@yahoo.de>
2021-04-26 11:57:48 +0200

    ipc appendix

--- v1_dataTransformation.py
+++ v1_dataTransformation.py
@@ -11,13 +11,16 @@ import re
 directory = 'D:/Universitaet Mannheim/MMDS 7. Semester/Master Thesis/Outline/Data/Cleaning Robots/'
 
 patent_topicDist    = pd.read_csv(directory + 'patent_topicDist.csv', quotechar='"', skipinitialspace=True)
+patent_IPC          = pd.read_csv(directory + 'cleaning_robot_EP_patents_IPC.csv', quotechar='"', skipinitialspace=True)
+parent              = pd.read_csv(directory + 'cleaning_robot_EP_backward_citations.csv', quotechar='"', skipinitialspace=True)
+parent_IPC          = pd.read_csv(directory + 'cleaning_robot_EP_backward_citations_IPC.csv', quotechar='"', skipinitialspace=True)
 #topics              = pd.read_csv(directory + 'patent_topics.csv', quotechar='"', skipinitialspace=True)
-#parent              = pd.read_csv(directory + 'cleaning_robot_EP_backward_citations.csv', quotechar='"', skipinitialspace=True)
 
 patent_topicDist    = patent_topicDist.to_numpy()
+patent_IPC          = patent_IPC.to_numpy()
+parent              = parent.to_numpy()
+parent_IPC          = parent_IPC.to_numpy()
 #topics              = topics.to_numpy()
-#parent              = parent.to_numpy()
-
 
 
 #--- Transformation of topic representation ---#
@@ -29,7 +32,7 @@ patent_topicDist    = patent_topicDist.to_numpy()
 # three columns for each topic represented in a patent          topic_id, topic_name, topic_coverage, topic_id, ... desc ordered by coverage
 # Note: Topic name not inserted yet. 'None' value in place
 
-
+'''
 ### Eliminating round brackets ###
 topic_reg_list = []
 
@@ -70,11 +73,11 @@ for i in topic_reg_list:
         tuple = (i[j], i[j + 1])
         tuple_list.append(tuple)
 
-    '''
+    ''''''
     ggg = patent_topicDist[c, 9]
     print(tuple_list)                      
     print(ggg)
-    '''
+    ''''''
     # todo this is the same except for the ''. upper code seems kinda redundant
 
     # Sort by coverage
@@ -99,10 +102,118 @@ for i in topic_reg_list:
     c = c + 1
 
 
-#--- Check and save transformation ---#
+#--- Check transformation ---#
 
 #print(np.shape(patent_topicDist_filled))              # (3781, 52)
 #print(sum(x is not None for x in patent_topicDist_filled[:,51])) # check if all created columns are used (they are)
-pd.DataFrame(patent_topicDist_filled).to_csv(directory + 'patent_topicDist_transf.csv', index=None)
 
-# todo append this to lda
+'''
+
+
+#--- Append IPC ---#
+
+patent_topicDist_transf    = pd.read_csv(directory + 'patent_topicDist_transf.csv', quotechar='"', skipinitialspace=True)
+patent_topicDist_transf = patent_topicDist_transf.to_numpy()
+
+#print(patent_topicDist_transf)
+#print(patent_IPC)
+
+
+### Prepare new array ###
+val, count = np.unique(patent_topicDist_transf[:,0], return_counts=True)
+
+#print(len(val))                            # 3781
+#print(len(patent_topicDist_transf))        # 3781 all ids of patent_topicDist_transf are unique
+
+val, count = np.unique(patent_IPC[:,0], return_counts=True)
+
+#print(len(val))                             # 3844
+#print(len(patent_IPC))                      # 9596
+#print(max(count))                           # 13
+# 13 * 3 (relevant columns in patent_IPC) = 39 additional columns are needed in the new array
+
+new_space_needed = max(count)*3              # 39
+
+patent_join = np.empty((np.shape(patent_topicDist_transf)[0],np.shape(patent_topicDist_transf)[1]+new_space_needed), dtype=object)
+patent_join[:,:-new_space_needed] = patent_topicDist_transf
+
+#print(patent_join.T)
+#print(np.shape(patent_topicDist_transf))    # (3781, 52)
+#print(np.shape(patent_join))                # (3781, 91)
+
+#print((patent_IPC))
+
+count_list = []
+
+
+for i in patent_IPC:
+
+
+    if i[0] in patent_join[:,0]:
+        count = count_list.count(i[0])
+        #print(count)
+
+        if patent_join[patent_join[:,0] == i[0],-(new_space_needed+count)] == None:
+            patent_join[patent_join[:,0] == i[0],-(new_space_needed+count)]         = i[1]
+            patent_join[patent_join[:,0] == i[0],-(new_space_needed+count-1)]       = i[2]
+            patent_join[patent_join[:,0] == i[0],-(new_space_needed+count-2)]       = i[3]
+
+    count_list.append(i[0])
+
+
+#print(patent_join.T[new_space_needed:new_space_needed+5,:])
+print(patent_join[:,51])
+print(len(patent_join))
+print(max(patent_join[:,51]))
+print(sum(x is not None for x in patent_join[:,51])) # check if all created columns are used (they are)
+
+newlist = [x for x in patent_join[:,50] if np.isnan(x) == False]
+print(newlist)
+
+'''
+### Keep patent_IPC only if ids in patent_join ###
+
+#patent_IPC_clean = patent_IPC[patent_IPC[:,0] in patent_join[:,0] ]
+
+removal_list = []
+for i in patent_IPC[:,0]:
+    if i not in patent_join[:,0]:
+        removal_list.append(i)
+
+print(len(removal_list))
+print(len(patent_IPC))
+#patent_IPC_clean = np.delete(patent_IPC, removal_list)
+#print(len(patent_IPC_clean))
+'''
+'''
+
+''''''
+comp_list = []
+for i in range(len(patent)):
+    if patent[i,0] in patent_ipc[:,0]:
+        comp_list.append(patent[i,0])
+
+print(len(comp_list))''''''
+
+
+patent_helper = np.empty((np.shape(patent_topicDist_transf)[0],max(count)*3+1), dtype=object)
+
+patent_helper[:,0] = patent_topicDist_transf[:,0]
+
+#print(patent_helper.T)
+
+#for i in patent[:,0]:
+'''
+
+
+
+
+
+
+
+
+#--- Save transformation and IPC appendix---#
+
+#pd.DataFrame(patent_topicDist_filled).to_csv(directory + 'patent_topicDist_transf.csv', index=None)
+
+
--- v1_slidingWindows.py
+++ v1_slidingWindows.py
@@ -14,6 +14,10 @@ patent_topicDist = pd.read_csv( directory + 'patent_topicDist_transf.csv', quote
 patent_topicDist = patent_topicDist.to_numpy()
 #parent = parent.to_numpy()
 
+window90by1_bool = False
+window60by1_bool = False
+window90by7_bool = False
+window60by7_bool = False
 
 
 #--- Overview ---#
@@ -44,129 +48,138 @@ patent_time_unique = np.unique(patent_time)                                     
 patent_time_unique_filled = np.arange(np.min(patent_time_unique), np.max(patent_time_unique))                               # 6027
 patent_time_unique_filled_90 = patent_time_unique_filled[patent_time_unique_filled <= max(patent_time_unique_filled)-90]    # 5937
 
-window90by1 = {}
-len_window = []
+if window90by1_bool == True:
 
-c = 0
-for i in patent_time_unique_filled_90:
-    lower_limit = i
-    upper_limit = i + 90
+    window90by1 = {}
+    len_window = []
 
-    patent_window = patent_topicDist[(patent_topicDist[:, 3].astype('datetime64') < upper_limit) & (patent_topicDist[:, 3].astype('datetime64') >= lower_limit)]
-    len_window.append(len(patent_window))
-
-    window90by1['window_{0}'.format(c)] = patent_window
-
-    if c % 100 == 0:
-        print(c, " / ", len(patent_time_unique_filled_90))
-    #if i >= 100:
-        #break
-
-    c = c + 1
-
-#print(len(window90by1))                     # 5937 windows
-#print(sum(len_window)/len(len_window))      # on average 56.253326595923866 patents per window
-
-filename = 'window90by1'
-outfile = open(filename,'wb')
-pk.dump(window90by1,outfile)
-outfile.close()
-
-
-### 60 days sliding by 1 day ###
-
-patent_time_unique_filled_60 = patent_time_unique_filled[patent_time_unique_filled <= max(patent_time_unique_filled)-60]
-
-window60by1 = {}
-len_window = []
-
-c = 0
-for i in patent_time_unique_filled_60:
-    lower_limit = i
-    upper_limit = i + 60
-
-    patent_window = patent_topicDist[(patent_topicDist[:, 3].astype('datetime64') < upper_limit) & (patent_topicDist[:, 3].astype('datetime64') >= lower_limit)]
-    len_window.append(len(patent_window))
-
-    window60by1['window_{0}'.format(c)] = patent_window
-
-    if c % 100 == 0:
-        print(c, " / ", len(patent_time_unique_filled_60))
-    #if i >= 100:
-        #break
-    c = c+1
-
-#print(len(window60by1))                        # 5967 windows
-#print(sum(len_window)/len(len_window))         # on average 37.50477626948215 patents per window
-
-filename = 'window60by1'
-outfile = open(filename,'wb')
-pk.dump(window60by1,outfile)
-outfile.close()
-
-
-### 90 days sliding by 7 day ###
-
-window90by7 = {}
-len_window = []
-
-c = 0
-for i in patent_time_unique_filled_90:
-
-    if c % 7 == 0:
+    c = 0
+    for i in patent_time_unique_filled_90:
         lower_limit = i
         upper_limit = i + 90
 
         patent_window = patent_topicDist[(patent_topicDist[:, 3].astype('datetime64') < upper_limit) & (patent_topicDist[:, 3].astype('datetime64') >= lower_limit)]
         len_window.append(len(patent_window))
 
-        window90by7['window_{0}'.format(c)] = patent_window
+        window90by1['window_{0}'.format(c)] = patent_window
 
-    if c % 100 == 0:
-        print(c, " / ", len(patent_time_unique_filled_90))
-    #if i >= 100:
-        #break
-    c = c+1
+        if c % 100 == 0:
+            print(c, " / ", len(patent_time_unique_filled_90))
+        #if i >= 100:
+            #break
 
+        c = c + 1
 
-#print(len(window90by7))                         # 849 windows
-#print(sum(len_window)/len(len_window))          # on average 56.849234393404004 patents per window
+    #print(len(window90by1))                     # 5937 windows
+    #print(sum(len_window)/len(len_window))      # on average 56.253326595923866 patents per window
 
-filename = 'window90by7'
-outfile = open(filename,'wb')
-pk.dump(window90by7,outfile)
-outfile.close()
-
-
-
-### 60 days sliding by 7 day ###
+    filename = 'window90by1'
+    outfile = open(filename,'wb')
+    pk.dump(window90by1,outfile)
+    outfile.close()
 
 
-window60by7 = {}
-len_window = []
+### 60 days sliding by 1 day ###
 
-c = 0
-for i in patent_time_unique_filled_60:
+patent_time_unique_filled_60 = patent_time_unique_filled[patent_time_unique_filled <= max(patent_time_unique_filled)-60]
 
-    if c % 7 == 0:
+if window60by1_bool == True:
+
+    window60by1 = {}
+    len_window = []
+
+    c = 0
+    for i in patent_time_unique_filled_60:
         lower_limit = i
         upper_limit = i + 60
 
         patent_window = patent_topicDist[(patent_topicDist[:, 3].astype('datetime64') < upper_limit) & (patent_topicDist[:, 3].astype('datetime64') >= lower_limit)]
         len_window.append(len(patent_window))
 
-        window60by7['window_{0}'.format(c)] = patent_window
+        window60by1['window_{0}'.format(c)] = patent_window
 
-    if c % 100 == 0:
-        print(c, " / ", len(patent_time_unique_filled_60))
-    #if i >= 100:
-        #break
-    c = c+1
+        if c % 100 == 0:
+            print(c, " / ", len(patent_time_unique_filled_60))
+        #if i >= 100:
+            #break
+        c = c+1
 
-#print(len(window60by7))                         # 853 windows
-#print(sum(len_window)/len(len_window))          # on average 39.35873388042204 patents per window
+    #print(len(window60by1))                        # 5967 windows
+    #print(sum(len_window)/len(len_window))         # on average 37.50477626948215 patents per window
 
-filename = 'window60by7'
-outfile = open(filename,'wb')
-pk.dump(window60by7,outfile)
-outfile.close()
+    filename = 'window60by1'
+    outfile = open(filename,'wb')
+    pk.dump(window60by1,outfile)
+    outfile.close()
+
+
+### 90 days sliding by 7 day ###
+
+
+if window90by7_bool == True:
+
+    window90by7 = {}
+    len_window = []
+
+    c = 0
+    for i in patent_time_unique_filled_90:
+
+        if c % 7 == 0:
+            lower_limit = i
+            upper_limit = i + 90
+
+            patent_window = patent_topicDist[(patent_topicDist[:, 3].astype('datetime64') < upper_limit) & (patent_topicDist[:, 3].astype('datetime64') >= lower_limit)]
+            len_window.append(len(patent_window))
+
+            window90by7['window_{0}'.format(c)] = patent_window
+
+        if c % 100 == 0:
+            print(c, " / ", len(patent_time_unique_filled_90))
+        #if i >= 100:
+            #break
+        c = c+1
+
+
+    #print(len(window90by7))                         # 849 windows
+    #print(sum(len_window)/len(len_window))          # on average 56.849234393404004 patents per window
+
+    filename = 'window90by7'
+    outfile = open(filename,'wb')
+    pk.dump(window90by7,outfile)
+    outfile.close()
+
+
+
+### 60 days sliding by 7 day ###
+
+
+if window60by7_bool == True:
+
+    window60by7 = {}
+    len_window = []
+
+    c = 0
+    for i in patent_time_unique_filled_60:
+
+        if c % 7 == 0:
+            lower_limit = i
+            upper_limit = i + 60
+
+            patent_window = patent_topicDist[(patent_topicDist[:, 3].astype('datetime64') < upper_limit) & (patent_topicDist[:, 3].astype('datetime64') >= lower_limit)]
+            len_window.append(len(patent_window))
+
+            window60by7['window_{0}'.format(c)] = patent_window
+
+        if c % 100 == 0:
+            print(c, " / ", len(patent_time_unique_filled_60))
+        #if i >= 100:
+            #break
+        c = c+1
+
+    #print(len(window60by7))                         # 853 windows
+    #print(sum(len_window)/len(len_window))          # on average 39.35873388042204 patents per window
+
+    filename = 'window60by7'
+    outfile = open(filename,'wb')
+    pk.dump(window60by7,outfile)
+    outfile.close()
