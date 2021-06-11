00e92e099f7c70a844887cb59a5954f1b827b423
Alexander Haberling <alexanderhaberling@yahoo.de>
2021-06-11 14:58:50 +0200

    rewriting network construction. user specific weighting function up next

--- v3_wholeNetworkConstruc.py
+++ v3_wholeNetworkConstruc.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     ### Plain citation nodes ###
 
     node_plain = patent_lda_ipc[:,0]
-    print(len(node_plain))
+    #print(len(node_plain))                         # 3781
 
 
     ### Plain citation edges ###
@@ -139,3 +139,91 @@ if __name__ == '__main__':
     print('Number of edges in plain: ', plain.number_of_edges())
 
 
+#--- Bipartite ---#
+
+    # 0 = top ~ topics
+
+    bipart.add_nodes_from(node_plain, bipartite=1)
+    nx.set_node_attributes(bipart, nested_dic)
+
+    topicNode_list = ['topic_{0}'.format(i) for i in range(len(topics))]
+    #print(topicNode_list)
+    #print(len(topicNode_list))                                      # 325
+
+    bipart.add_nodes_from(topicNode_list, bipartite=0)
+    #print(bipart.number_of_nodes())                                 # 4106 = 3781 + 325
+
+
+    ### edges ###
+
+
+    bipart_edges = patent_lda_ipc[:,np.r_[0, 9:18]]         # first three topics
+
+    c = 0
+    for i in bipart_edges.T[1]:
+        if np.isfinite(i):
+            bipart_edges[c,1] = 'topic_{0}'.format(int(i))
+        c = c + 1
+
+    c = 0
+    for i in bipart_edges.T[4]:
+        if np.isfinite(i):
+            bipart_edges[c, 4] = 'topic_{0}'.format(int(i))
+        c = c + 1
+
+
+    c = 0
+    for i in bipart_edges.T[7]:
+        if np.isfinite(i):
+            bipart_edges[c,7] = 'topic_{0}'.format(int(i))
+        c = c + 1
+
+
+
+    topic1_edges = [(i[0], i[1], {'Weight_1': i[3]}) for i in bipart_edges]
+    topic2_edges = [(i[0], i[4], {'Weight_2': i[6]}) for i in bipart_edges]
+    topic3_edges = [(i[0], i[7], {'Weight_3': i[9]}) for i in bipart_edges]
+
+    #print(bipart_edges[0])
+    #print(topic1_edges)
+    #print(topic2_edges)
+
+    #print(len(topic1_edges))
+
+    topic1_edges_clear = list(filter(lambda x: x[1] == x[1], topic1_edges))
+    topic2_edges_clear = list(filter(lambda x: x[1] == x[1], topic2_edges))
+    topic3_edges_clear = list(filter(lambda x: x[1] == x[1], topic3_edges))
+
+    #print(topic1_edges_clear)
+    #print(len(topic1_edges_clear))
+
+    print(len(bipart.nodes))
+    print(len(bipart.edges))
+
+    bipart.add_edges_from(topic1_edges_clear)
+    bipart.add_edges_from(topic2_edges_clear)
+    bipart.add_edges_from(topic3_edges_clear)
+    print(len(bipart.nodes))
+    print(len(bipart.edges))
+
+    print(nx.is_connected(bipart))
+
+    #bottom_nodes, top_nodes = nx.algorithms.bipartite.sets(bipart) # not working, because not connected
+
+    top_nodes = {n for n, d in bipart.nodes(data=True) if d["bipartite"] == 0}
+    bottom_nodes = set(bipart) - top_nodes
+
+    print(top_nodes)
+    print(len(top_nodes))
+    print(bottom_nodes)
+    print(len(bottom_nodes))
+
+    print(nx.algorithms.bipartite.density(bipart, top_nodes))
+    print(round(nx.algorithms.bipartite.density(bipart, top_nodes), 2))
+    print(nx.algorithms.bipartite.density(bipart, bottom_nodes))
+    print(round(nx.algorithms.bipartite.density(bipart, bottom_nodes), 2))
+
+    topic_g_unweig = nx.algorithms.bipartite.projected_graph(bipart, top_nodes)
+    topicSim_g_unweig = nx.algorithms.bipartite.projected_graph(bipart, bottom_nodes)
+
+    # nx.write_gml(plain, r'D:\Universitaet Mannheim\MMDS 7. Semester\Master Thesis\Outline\Data\Cleaning Robots\plain.gml')
