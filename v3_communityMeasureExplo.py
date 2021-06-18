a59aae9e83f0ff4d6d0e61fcb168316bd1c75cab
Alexander Haberling <alexanderhaberling@yahoo.de>
2021-06-18 14:37:52 +0200

    sliding window approach on network creation done, community detection next, developing ideas

--- v3_communityMeasureExplo.py
+++ v3_communityMeasureExplo.py
@@ -58,14 +58,90 @@ if __name__ == '__main__':
 
         #print('+++++++++++++++++++++++++++++++++++++++++')
 
-        lp_commu[window_id] = lp
+        lp_commu[window_id] = list(lp)
 
-    print(lp_commu)
+    #print(lp_commu)
+    #print(lp_commu['window_0'])
+    #for i in lp_commu['window_0']:          # {291407609} {290720124, 290720623}
+        #print(i)
+
+    for i in range(0, len(lp_commu)-1):
+
+        #print(lp_commu['window_{0}'.format(i)])
+        all_ids_t = lp_commu['window_{0}'.format(i)]
+
+        all_ids_t = [item for sublist in all_ids_t for item in sublist]
+
+        #print(all_ids_t)
+
+        #print(len(all_ids_t))
+        #print(len(np.unique(all_ids_t)))
+        #print(len(all_ids_t) == len(np.unique(all_ids_t)))
+
+
+    # 0. delete all communities that are not a least of size x (for now 2)
+    # 0.5 take communities and calculate the most relevant topic/s in them, with frequency
+
+    # 1. take normal sliding window
+    # 2. see which patents are new every t
+    # 3. see if the new patents connects to patents of overall two or more different communities at t-1
+    # 4. if some, save window_id, bridging patent, community endpoint 1, community topics 1, community endpoint 2, community topics 2,
+    # 5.  check if the number of bridging patents between two communities meets a certain threshold ( x% of all new papers or x% of community size 1 + community size 2, or the average of the two)
+    # Measure of diffusion a lot more difficult. One would have to follow the communities over t. if it is possible to identify communities that are stable over t, then
+    # it would be possible to assess for how many t's a threshold of bridging patents is meet. This seems too be to much for my thesis. Alternatively one could check how many citations
+    # a bridging patent gets over time, but this is contrary to the reason why we are doing it with topics in the first place, and only a lazy approximation
+
+    # Overlapping communities:
+
+    # same approach
+    # calculate communities for every window
+    # check if there are overlapping communities. check if they meet a threshold (x% of all new patets vs x% of community sizes) if so then these patents that are overlapping are recombining information. Same limitation for diffusion: to evaluate how long the threshold is meet
+    # one has to establish the consistency / stability of communtiies over time.
+
+    # how to check stability over time of communities:
+    # 0. establish core of community (node with most internal connection within the community) (one core for each window and each communitiy)
+    # if the core drops out, then look for the second/third/... most connected node in the network, that exists in t+1
+    # 1. how many patents that left the community also left the graph in general
+    # 2. how many patents that joined the community also joined the graph in general
+    #
+    # 3. Can I develop a stability measure when doing this for all communities (how much movement is there between communities)
+    # is there a already established measure i can use? I guess smth like modularity onyl talks about the quality of the partition, not about the sbaility over time.
+
+
+
+    '''
+    #test = [x for x in lp_commu['window_0']]
+    #print(test)
+
+    x = {290720124}
+    #rnd_bool = x in lp_commu['window_0']
+    #rnd_bool = x.issubset(lp_commu['window_0'])
+
+    test = [x for set in lp_commu['window_0'] if x.issubset(set)]
+
+    print(test)
+    '''
+
+
+    # is the similarity between patens changing over time? No, because chnage in the network is only caused by added patents. these patents instantly bring their
+    # topic affiliatio. this affiliation is not changing over time, since the abstract and the lda model is not changing. this leads to no change in the patent similarity as well.
+    # this means that all networks are not dynamic, expect for the topic occurance network.
+
+    #print(rnd_bool)
+
+    #s = (val for val in range(10))
+    #print(1 in s)
+
+    #x = {range(10) }
+    #print(x)
+    '''
+    if  in lp_commu['window_0']:
+        print('yeees')
+    else:
+        print('noooo')
+
     print(lp_commu['window_0'])
-    for i in lp_commu['window_0']:
-        print(i)
-
-
+    '''
     # non-overlapping:
     # label propagation weighted
     # leiden weighted (what is 'initial_membership' parameter? Important?)
